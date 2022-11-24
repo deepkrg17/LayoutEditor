6749e5b1cfb3bcbaff577d11d465facc698440f9
Vivek Kumar Sahani <itsvks19@gmail.com>
2022-11-24 15:48:11 +0530

    Added Dynamic Colors

--- app/src/main/java/com/itsvks/layouteditor/LayoutEditor.java
+++ app/src/main/java/com/itsvks/layouteditor/LayoutEditor.java
@@ -2,19 +2,29 @@ package com.itsvks.layouteditor;
 
 import android.app.Application;
 import android.content.Context;
+import android.os.Build;
+
+import com.google.android.material.color.DynamicColors;
 
 public class LayoutEditor extends Application {
-    
+
     private static LayoutEditor sIstance;
-    
+
     @Override
     public void onCreate() {
         super.onCreate();
         sIstance = this;
         Thread.setDefaultUncaughtExceptionHandler(new CrashHandler(this));
+        if (isAtLeastS() && DynamicColors.isDynamicColorAvailable()) {
+            DynamicColors.applyToActivitiesIfAvailable(this);
+        }
     }
-    
+
     public static Context getContext() {
         return sIstance.getApplicationContext();
+    }
+
+    public static boolean isAtLeastS() {
+        return Build.VERSION.SDK_INT >= Build.VERSION_CODES.S;
     }
 }
