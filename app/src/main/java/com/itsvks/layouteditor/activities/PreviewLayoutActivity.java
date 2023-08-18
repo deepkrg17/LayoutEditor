50697f1671bd29408c4cec45ad86ea50bd2cda44
Vivek <itsvks19@gmail.com>
2023-08-18 21:35:21 +0530

    Add "android:weightSum" attribute in LinearLayout

--- app/src/main/java/com/itsvks/layouteditor/activities/PreviewLayoutActivity.java
+++ app/src/main/java/com/itsvks/layouteditor/activities/PreviewLayoutActivity.java
@@ -26,33 +26,5 @@ public class PreviewLayoutActivity extends BaseActivity {
     parser.parseFromXml(project.getLayout(), this);
 
     binding.getRoot().addView(parser.getRoot());
-    //    binding.designEditor.addView(parser.getRoot());
-    //    binding
-    //        .getRoot()
-    //        .setOnClickListener(
-    //            v -> {
-    //              switch (binding.designEditor.getViewType()) {
-    //                case BLUEPRINT:
-    //                  binding.designEditor.setViewType(DesignEditor.ViewType.DESIGN);
-    //                  break;
-    //                case DESIGN:
-    //                  binding.designEditor.setViewType(DesignEditor.ViewType.BLUEPRINT);
-    //                  break;
-    //              }
-    //              switch (binding.designEditor.getDeviceConfiguration().getSize()) {
-    //                case SMALL:
-    //                  binding.designEditor.resizeLayout(
-    //                      new DeviceConfiguration(DeviceSize.MEDIUM));
-    //                  break;
-    //                case MEDIUM:
-    //                  binding.designEditor.resizeLayout(
-    //                      new DeviceConfiguration(DeviceSize.LARGE));
-    //                  break;
-    //                case LARGE:
-    //                  binding.designEditor.resizeLayout(
-    //                      new DeviceConfiguration(DeviceSize.SMALL));
-    //                  break;
-    //              }
-    //            });
   }
 }
